dc884622d8f745255daa0efb4f6490779c660e5b
Scott Ferguson <124411070+ferg-essay@users.noreply.github.com>
2024-01-31 18:53:47 -0800

    hind_move cleanup removing RandomWalk

--- crates/vertebrate/src/hind_motor/hind_move.rs
+++ crates/vertebrate/src/hind_motor/hind_move.rs
@@ -7,11 +7,6 @@ use util::random::{random, random_normal, random_pareto, random_uniform};
 
 
 pub struct HindMove {
-    left60: Turn,
-    right60: Turn,
-    left120: Turn,
-    _right120: Turn,
-
     move_commands: Command<MoveCommand>,
     turn_commands: Command<TurnCommand>,
 
@@ -26,26 +21,16 @@ pub struct HindMove {
     approach_left: DecayValue,
     approach_right: DecayValue,
     approach_forward: DecayValue,
-    // approach_dir: DirVector,
 
     avoid_left: DecayValue,
     avoid_right: DecayValue,
     avoid_forward: DecayValue,
-    // avoid_dir: DirVector,
 
     is_last_turn: bool,
-
-    // action_kind: BodyAction,
-
-    is_first: bool,
 }
 
 impl HindMove {
     const HALF_LIFE : f32 = 0.2;
-    const LEN_LOW : f32 = 1.;
-    const LEN_HIGH : f32 = 5.;
-    const LEN_TURN : f32 = 1.;
-    const ALPHA : f32 = 2.;
 
     const TURN_MEAN : f32 = 60.;
     const TURN_STD : f32 = 15.;
@@ -53,13 +38,6 @@ impl HindMove {
 
     fn new() -> Self {
         Self {
-            left60: Turn::new(Angle::Deg(-60.), Angle::Deg(15.)),
-            right60: Turn::new(Angle::Deg(60.), Angle::Deg(15.)),
-
-            left120: Turn::new(Angle::Deg(-120.), Angle::Deg(60.)),
-            _right120: Turn::new(Angle::Deg(120.), Angle::Deg(60.)),
-
-            //random_walk: RandomWalk::new(),
             move_commands: Command::new(),
             turn_commands: Command::new(),
 
@@ -80,8 +58,6 @@ impl HindMove {
             avoid_forward: DecayValue::new(Self::HALF_LIFE),
 
             is_last_turn: false,
-
-            is_first: true,
         }
     }
 
@@ -163,36 +139,26 @@ impl HindMove {
     }
 
     fn move_command(&mut self, event: &MoveCommand) {
-        //if self.is_first {
-        //    self.is_first = false;
-        //    self.explore.pre_update();
-        //}
-
         match event {
             // explore/speed modes
             MoveCommand::Seek => {
                 self.action_kind = self.action_kind.explore();
-                //self.random_walk.approach();
                 self.roam.set_max(1.);
             }
             MoveCommand::Avoid => {
                 self.action_kind = self.action_kind.explore();
-                //self.random_walk.avoid();
                 self.avoid.set_max(1.);
             }
             MoveCommand::Normal => {
                 self.action_kind = self.action_kind.explore();
-                //self.random_walk.normal();
                 self.roam.set_max(1.);
             }
             MoveCommand::Roam => {
                 self.action_kind = self.action_kind.explore();
-                //self.random_walk.roam();
                 self.roam.set_max(1.);
             }
             MoveCommand::Dwell => {
                 self.action_kind = self.action_kind.explore();
-                //self.random_walk.dwell();
                 self.dwell.set_max(1.);
             }
             MoveCommand::Stop => {
@@ -231,13 +197,11 @@ impl HindMove {
             TurnCommand::AvoidVector(vector) => {
                 self.action_kind = self.action_kind.seek();
                 self.add_avoid(vector)
-                // self.roam();
             },
             
             TurnCommand::ApproachVector(vector) => {
                 self.action_kind = self.action_kind.seek();
                 self.add_approach(vector)
-                // self.approach(); 
             },
 
             TurnCommand::AvoidUTurn => {
@@ -263,45 +227,6 @@ impl HindMove {
         self.action = self.update_action();
 
         body.set_action(self.action.kind, self.action.speed, self.action.turn);
-
-        /*
-        match self.action_kind {
-            ActionKind::Stop => {
-                if body.speed() > 0. {
-                    body.stop();
-                }
-            },
-            ActionKind::Roam => {
-                self.action = self.random_walk.update();
-
-                body.roam(self.action.speed, self.action.turn);
-            },
-            ActionKind::Seek => {
-                self.action = self.random_walk.update();
-
-                body.seek(self.action.speed, self.action.turn);
-            },
-            ActionKind::StrongAvoidLeft => {
-                self.action = Action::new(BodyAction::Avoid, 0.25, 1., self.right60.angle());
-
-                body.avoid(1., self.right60.angle());
-            },
-            ActionKind::StrongAvoidRight => {
-                body.avoid(1., self.left60.angle());
-            },
-            ActionKind::StrongAvoidBoth => {
-                body.avoid(1., self.left120.angle());
-                //todo!();
-                /*
-                if random_normal() < 0. {
-                    body.locomotion_mut().avoid(self.left120.action(1.));
-                } else {
-                    body.locomotion_mut().avoid(self.right120.action(1.));
-                }
-                */
-            },
-        }
-        */
     }
 
     fn add_avoid(&mut self, avoid_dir: DirVector) {
@@ -326,30 +251,6 @@ impl HindMove {
 
             self.seek.set_max(1.);
         }
-    }
-
-    fn avoid_forward(&self) -> f32 {
-        self.avoid_forward.value()
-    }
-
-    fn avoid_left(&self) -> f32 {
-        self.avoid_left.value()
-    }
-
-    fn avoid_right(&self) -> f32 {
-        self.avoid_right.value()
-    }
-
-    fn forward_delta(&self) -> f32 {
-        0.5 * (self.avoid_forward.value() - self.approach_forward.value() + 1.)
-    }
-
-    fn left_delta(&self) -> f32 {
-        0.5 * (self.avoid_left.value() - self.approach_left.value() + 1.)
-    }
-
-    fn right_delta(&self) -> f32 {
-        0.5 * (self.avoid_right.value() - self.approach_right.value() + 1.)
     }
 
     fn update_action(
@@ -399,12 +300,6 @@ impl HindMove {
         &mut self,
         move_command: MoveCommand,
     ) -> Action {
-        //body.set_action(self.action);
-
-        //if self.action.pre_update() {
-        //    return None;
-        //}
-
         let speed = move_command.speed();
         let len = 1.;
 
@@ -455,8 +350,6 @@ impl MoveCommand {
     const DWELL_HIGH : f32 = 1.;
     
     const ALPHA : f32 = 2.;
-
-    const LEN_TURN : f32 = 1.;
 
     fn run_len(&self) -> f32 {
         match self {
@@ -598,301 +491,8 @@ impl ActionKind {
     }
 }
 
-struct RandomWalk {
-    speed: f32,
-
-    alpha: f32,
-
-    len_low: f32,
-    len_high: f32,
-    turn_len: f32,
-
-    turn_mean: f32,
-    turn_std: f32,
-
-    approach_left: f32,
-    approach_right: f32,
-    approach_forward: f32,
-    approach_dir: DirVector,
-
-    avoid_left: f32,
-    avoid_right: f32,
-    avoid_forward: f32,
-    avoid_dir: DirVector,
-
-    is_last_turn: bool,
-
-    action_kind: BodyAction,
-}
-
-impl RandomWalk {
-    const LEN_LOW : f32 = 1.;
-    const LEN_HIGH : f32 = 5.;
-    const LEN_TURN : f32 = 1.;
-    const ALPHA : f32 = 2.;
-
-    const TURN_MEAN : f32 = 60.;
-    const TURN_STD : f32 = 15.;
-
-    fn new() -> Self {
-        RandomWalk {
-            speed: 1.,
-
-            len_low: Self::LEN_LOW,
-            len_high: Self::LEN_HIGH,
-            alpha: Self::ALPHA,
-            turn_len: 1.,
-
-            turn_mean: Self::TURN_MEAN,
-            turn_std: Self::TURN_STD,
-
-            approach_left: 0.,
-            approach_right: 0.,
-            approach_forward: 0.,
-            approach_dir: DirVector::zero(),
-
-            avoid_left: 0.,
-            avoid_right: 0.,
-            avoid_forward: 0.,
-            avoid_dir: DirVector::zero(),
-
-            is_last_turn: false,
-
-            // action: Action::none(),
-            action_kind: BodyAction::Roam,
-        }
-    }
-
-    fn pre_update(&mut self) {
-        self.roam();
-
-        self.approach_left = 0.;
-        self.approach_right = 0.;
-        self.approach_forward = 0.;
-        self.approach_dir = DirVector::zero();
-
-        self.avoid_left = 0.;
-        self.avoid_right = 0.;
-        self.avoid_forward = 0.;
-        self.avoid_dir = DirVector::zero();
-    }
-
-    fn normal(&mut self) {
-        self.roam();
-    }
-
-    fn approach(&mut self) {
-        self.dwell();
-    }
-
-    fn roam(&mut self) {
-        self.speed = 1.;
-
-        self.len_low = Self::LEN_LOW;
-        self.len_high = Self::LEN_HIGH;
-        self.alpha = Self::ALPHA;
-        self.turn_len = Self::LEN_TURN;
-
-        self.turn_mean = Self::TURN_MEAN;
-        self.turn_std = Self::TURN_STD;
-
-        self.action_kind = BodyAction::Roam;
-    }
-
-    fn dwell(&mut self) {
-        self.speed = 0.5;
-
-        self.len_low = Self::LEN_LOW;
-        self.len_high = Self::LEN_HIGH.min(2. * Self::LEN_LOW);
-        self.alpha = Self::ALPHA;
-
-        self.turn_mean = Self::TURN_MEAN;
-        self.turn_std = Self::TURN_STD;
-
-        self.action_kind = BodyAction::Dwell;
-    }
-
-    fn stop(&mut self) {
-        self.speed = 0.;
-
-        self.action_kind = BodyAction::None;
-    }
-
-    fn add_avoid(&mut self, avoid_dir: DirVector) {
-        if avoid_dir.value() > 0.05 {
-            let offset = 2. * avoid_dir.sin(); // * avoid_dir.value();
-
-            self.avoid_left = offset.clamp(0., 1.);
-            self.avoid_right = (- offset).clamp(0., 1.);
-            self.avoid_forward = avoid_dir.cos().clamp(0., 1.);
-            self.avoid_dir = avoid_dir;
-        }
-    }
-
-    fn add_approach(&mut self, approach_dir: DirVector) {
-        if approach_dir.value() > 0.01 {
-            let offset = 2. * approach_dir.dx(); // * approach_dir.value();
-
-            self.approach_left = (- offset).clamp(0., 1.);
-            self.approach_right = offset.clamp(0., 1.);
-            self.approach_forward = - approach_dir.dy().clamp(-1., 0.);
-            self.approach_dir = approach_dir;
-
-            self.action_kind = BodyAction::Seek;
-        }
-    }
-
-    fn avoid_forward(&self) -> f32 {
-        self.avoid_forward
-    }
-
-    fn avoid_left(&self) -> f32 {
-        self.avoid_left
-    }
-
-    fn avoid_right(&self) -> f32 {
-        self.avoid_right
-    }
-
-    fn avoid(&mut self) {
-        // prefer
-        //self.low = 2. * Self::HIGH;
-        //self.high = 2. * Self::HIGH;
-        //self.alpha = 1.;
-
-        //self.turn_mean = 2. * Self::TURN_MEAN;
-        //self.turn_std = Self::TURN_STD;
-
-        // self.low = Self::LOW;
-        // self.high = 2. * Self::HIGH;
-        // self.alpha = Self::ALPHA;
-
-        // self.turn_mean = 2. * Self::TURN_MEAN;
-        // self.turn_std = Self::TURN_STD;
-
-        // self.low = Self::LOW;
-        // self.high = 2. * Self::HIGH;
-        // self.alpha = 3.;
-
-        // self.turn_mean = 2. * Self::TURN_MEAN;
-        // self.turn_std = Self::TURN_STD;
-
-        self.len_low = Self::LEN_HIGH;
-        self.len_high = 2. * Self::LEN_HIGH;
-        self.alpha = Self::ALPHA;
-
-        self.turn_mean = 0.5 * Self::TURN_MEAN;
-        self.turn_std = 0.5 * Self::TURN_STD;
-    }
-
-    fn forward_delta(&self) -> f32 {
-        0.5 * (self.avoid_forward - self.approach_forward + 1.)
-    }
-
-    fn left_delta(&self) -> f32 {
-        0.5 * (self.avoid_left - self.approach_left + 1.)
-    }
-
-    fn right_delta(&self) -> f32 {
-        0.5 * (self.avoid_right - self.approach_right + 1.)
-    }
-
-    fn avoid_turn(&mut self) {
-        self.len_low = Self::LEN_LOW;
-        self.len_high = 0.5 * Self::LEN_HIGH;
-        self.alpha = 1.;
-
-        self.turn_mean = 2. * Self::TURN_MEAN;
-        self.turn_std = 3. * Self::TURN_STD;
-    }
-
-    fn _prefer(&mut self) {
-        self.len_low = Self::LEN_LOW;
-        self.len_high = Self::LEN_HIGH;
-        self.alpha = Self::ALPHA;
-    }
-
-    fn _is_turn(&self) -> bool {
-        self.is_last_turn
-    }
-
-    fn update(
-        &mut self,
-    ) -> Action {
-        //body.set_action(self.action);
-
-        //if self.action.pre_update() {
-        //    return None;
-        //}
-
-        let random = random();
-        let mut mean = self.turn_mean;
-        let mut std = self.turn_std;
-
-        let avoid_forward = self.avoid_forward + self.approach_forward;
-        if avoid_forward > 0. && random_normal().abs() < avoid_forward {
-            mean = 2. * Self::TURN_MEAN;
-            std = 3. * Self::TURN_STD;
-        }
-
-        let angle = mean + (random_normal() * std).clamp(-2. * std, 2. * std);
-
-        // bounded pareto as approximation of Levy walk
-        let low = self.len_low;
-        let high = self.len_high; // 4.;
-        let alpha = self.alpha;
-
-        let f = 4.;
-        let approach_left = (1. - f * self.approach_right - self.avoid_left).max(1.0e-6);
-        let approach_right = (1. - f * self.approach_left - self.avoid_right).max(1.0e-6);
-        let p_left = approach_left / (approach_left + approach_right).max(0.01);
-
-        let speed = self.speed;
-
-        // semi-brownian
-        if self.is_last_turn {
-            self.is_last_turn = false;
-
-            let len = random_pareto(low, high, alpha);
-
-            Action::new(self.action_kind, len, speed, Angle::Unit(0.))
-        } else if random <= p_left {
-            self.is_last_turn = true;
-
-            Action::new(self.action_kind, self.turn_len, speed, Angle::Deg(- angle))
-        } else {
-            self.is_last_turn = true;
-
-            Action::new(self.action_kind, self.turn_len, speed, Angle::Deg(angle))
-        }
-    }
-}
-
-struct Turn {
-    mean: Angle,
-    std: Angle,
-}
-
-impl Turn {
-    fn new(mean: Angle, std: Angle) -> Self {
-        Self {
-            mean,
-            std
-        }
-    }
-
-    fn angle(&self) -> Angle {
-        let mean = self.mean.to_unit();
-        let std = self.std.to_unit();
-
-        let angle = mean + std * random_normal().clamp(-2., 2.);
-
-        Angle::unit(angle)
-    }
-}
-
 #[derive(Clone, Debug)]
-pub struct Action {
+struct Action {
     kind: BodyAction,
     time: f32,
     speed: f32,
@@ -921,7 +521,7 @@ impl Action {
         self.time >= 1.0e-6
     }
 
-    fn update_stop(&mut self) {
+    fn _update_stop(&mut self) {
         self.speed -= 0.1;
 
         if self.speed <= 0. {
