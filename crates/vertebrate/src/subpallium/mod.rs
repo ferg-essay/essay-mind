cf13b50f8359884fdcc7c63612824084d1c3eafd
Scott Ferguson <124411070+ferg-essay@users.noreply.github.com>
2025-05-20 12:12:03 -0700

    add striatum mosaic

--- crates/vertebrate/src/subpallium/mod.rs
+++ crates/vertebrate/src/subpallium/mod.rs
@@ -1,3 +1,4 @@
+mod mosaic;
 mod striatum2;
 mod basal_forebrain;
 mod gate;
@@ -5,4 +6,5 @@ mod striatum;
 
 pub use basal_forebrain::{AttendId, AttendValue, BasalForebrain};
 pub use gate::{Gate, StriatumGate};
+pub use mosaic::{Mosaic, MosaicType};
 pub use striatum::{StriatumTimeout, StriatumValue, StriatumId, StriatumExclusive};
